3b73e2c29656077bed1825de30fec10aefc83d53
Faris_USD <faris.hameed@uniquesoftwaredev.com>
2017-06-17 14:57:36 -0700

    components,directives

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import { NgModule, ModuleWithProviders } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 import { SampleComponent } from "./sample.component";
 import { SampleDirective } from "./sample.directive";
 import { SamplePipe } from "./sample.pipe";
 import { SampleService } from "./sample.service";
+
+import { DataTable } from "./components/table/table.component";
+import { DataTableColumn } from "./components/column/column.component";
+import { DataTableRow } from "./components/row/row.component";
+import { DataTablePagination } from "./components/pagination/pagination.component";
+import { DataTableHeader } from "./components/header/header.component";
+
+export * from "./components/types";
+export * from "./tools/data-table-resource";
+
+import { PixelConverter } from "./utils/px";
+import { Hide } from "./utils/hide";
+import { MinPipe } from "./utils/min";
 
 export * from "./sample.component";
 export * from "./sample.directive";
@@ -11,14 +25,32 @@ export * from "./sample.pipe";
 export * from "./sample.service";
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [SampleComponent, SampleDirective, SamplePipe],
-  exports: [SampleComponent, SampleDirective, SamplePipe]
+  imports: [CommonModule, FormsModule],
+  declarations: [
+    DataTable,
+    DataTableColumn,
+    DataTableRow,
+    DataTablePagination,
+    DataTableHeader,
+    PixelConverter,
+    Hide,
+    MinPipe,
+    SampleComponent,
+    SampleDirective,
+    SamplePipe
+  ],
+  exports: [
+    DataTable,
+    DataTableColumn,
+    SampleComponent,
+    SampleDirective,
+    SamplePipe
+  ]
 })
-export class NgNoobTable {
+export class NgNoobTableModule {
   static forRoot(): ModuleWithProviders {
     return {
-      ngModule: NgNoobTable,
+      ngModule: NgNoobTableModule,
       providers: [SampleService]
     };
   }
